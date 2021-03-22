2f3af1981e2b876dfba2ba971dc22579de38b4a8
nijian <nijian@jiangduoduo.com>
2021-03-22 11:37:44 +0800

    commit

---
+++ test/weight.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from collections import Counter
+# l = [1, 2, 3, 4]
+# l_ = l[:3][-2:]
+# print(l_)
+# brush_10 = ["1","1","2"]
+# counter =  Counter(brush_10)
+# print(counter)
+#
+# import numpy as np
+#
+# nums = np.random.rand(4,3)
+# print(nums)
+# print (np.where(nums > 0.5, 1, 0))
+# import numpy as np
+# a = np.array([[1],[0],[0],[1]])
+# l = np.array([[1],[1] ,[0],[1]])
+# print((a==l).sum())
+
+import json
+l = list([1, 2, 3, 4])
+s = json.dumps(l)
+print(s)
+
+
+
+
